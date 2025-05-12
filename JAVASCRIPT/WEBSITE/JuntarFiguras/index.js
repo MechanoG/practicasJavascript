8449ec6edd8fe7800d04d6f4769fa22fe5ff65f6
MechanoG <glory_to_the_machine@protonmail.com>
2025-05-12 00:10:17 -0400

    JuntarFiguras.FigurasIniciales

--- JAVASCRIPT/WEBSITE/JuntarFiguras/index.js
+++ JAVASCRIPT/WEBSITE/JuntarFiguras/index.js
@@ -1,11 +1,67 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+//--Proyecto: Colocar las figuras correstas en su posicion
 
-ctx.fillStyle="red";
-ctx.fillRect(0, 0, 150, 75);
+//Se inicician los canvas de las figuras iniciales
+//Circulo, Cuadrado, Triangulo, Corazon Estrella Cruzz
+const greyCircle = document.getElementById("greyCircle");
+const greySquare = document.getElementById("greySquare");
+const greyTriangle = document.getElementById("greyTriangle");
+const greyHeart = document.getElementById("greyHeart");
+const greyStar = document.getElementById("greyStar");
+const greyCross = document.getElementById("greyCross");
 
-ctx.fillStyle="blue";
-ctx.fillRect(160, 85, 150, 75);
+//Colores
+const gris = "grey";
+const red = "red";
+const green = "green";
+const blue = "blue";
 
-ctx.fillStyle="green";
-ctx.fillRect(150, 75, 10, 10);
+
+function drawFigure(canvas){
+    if(!canvas){
+        alert("There is not such a canvas ");
+    }
+    
+    const id = canvas.id
+    const lapiz = canvas.getContext("2d");
+
+    switch (canvas.id) {
+        case "greyCircle":
+                drawCircle(canvas.width/2, canvas.height/2, lapiz, "grey");
+            break;
+        case "greySquare":
+                drawSquare(canvas.width, canvas.height,lapiz, "grey");
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function drawSquare(width, height, pincel, color ){
+    pincel.fillStyle = color;
+    pincel.fillRect(0,0,width,height);
+}
+
+function drawCircle(width, height, pincel, color){
+    pincel.beginPath();
+    pincel.arc(width, height, width, 0,  2 * Math.PI);   
+    pincel.fillStyle = color;
+    pincel.fill();
+    pincel.stroke();
+}
+/*
+drawFigure(greySquare);
+drawFigure(greyCircle);
+*/
+
+const lapiz = greyHeart.getContext("2d");
+lapiz.beginPath();
+lapiz.moveTo(150/2, 25);
+lapiz.bezierCurveTo(0, 36, 0, 125, 75, 150);
+lapiz.stroke();
+
+
+
+
+
+

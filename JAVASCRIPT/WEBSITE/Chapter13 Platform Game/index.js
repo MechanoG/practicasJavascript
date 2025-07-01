70e71bc44506a0c8564136211d67447c16ee6001
MechanoG <glory_to_the_machine@protonmail.com>
2025-06-30 23:37:26 -0400

    add Motion ann colission

--- JAVASCRIPT/WEBSITE/Chapter13 Platform Game/index.js
+++ JAVASCRIPT/WEBSITE/Chapter13 Platform Game/index.js
@@ -149,7 +149,7 @@ console.log(`${simpleLevel.width} by ${simpleLevel.heigt}`);
 //child nodes:
 function elt(name, attrs, ...children){
     let dom = document.createElement(name);
-    for (let attr of Objects.keys(attrs)){
+    for (let attr of Object.keys(attrs)){
         dom.setAttribute(attr, attrs[attr]);
     }
     for (let child of children){
@@ -184,7 +184,7 @@ function drawGrid(level){
 }
 
 function drawActors(actors){
-    return elt("div", {}, ...actors.maps(actor=>{
+    return elt("div", {}, ...actors.map(actor=>{
         let rect = elt("div", {class:`actor ${actor.type}`});
         rect.style.width = `${actor.size.x * scale}px`;
         rect.style.height = `${actor.size.y * scale}px`;
@@ -234,5 +234,63 @@ DOMDisplay.prototype.scrollPlayerIntoView = function(state){
 
 }
 
+//let simpleLevel = new Level(simpleLevelPlan);
 let display = new DOMDisplay(document.body, simpleLevel);
-display.syncState(State.start(simpleLevel))
+display.syncState(State.start(simpleLevel));
+
+//Motion and colission
+
+/*Method that tells wheteher a rectangle touces a grid
+element of the given tipe*/
+
+Level.prototype.touches = function (pos, size, type){
+    let xStart = Math.floor(pos.x);
+    let xEnd = Math.ceil(pos.x + size.x);
+    let yStart = Math.floor(pos.y);
+    let yEnd = Math.ceil(pos.y + size.y);
+
+    for (let y = yStart; y < yEnd; y++){
+        for(let x = xStart; x < xEnd; x++){
+            let isOutside = x < 0 || x >= this.width ||
+            y<0 || y>this.heigt;
+            let here = isOutside ? "wall" : this.rows[y][x]; 
+            if (here == type) return true;
+        }
+    }
+    return false;
+};
+
+State.prototype.update = function(time, keys){
+    let actors = this.actors
+        .map(actor => actor.update(time, this, keys));
+    let newState = new State(this.level, actors, this.status);
+
+    if  (newState.status !="playing") return newState;
+
+    let player = newState.player;
+    if(this.level.touches(player.pos, player.size, "lava")){
+        return new State(this.level, actors, "lost");
+    }
+
+    for (let actor of actors){
+        if (actor!= player && overlap(actor,player)){
+            newState = actor.collide(newState)
+        }
+    }
+    return newState;
+
+    function overlap(actor1, actor2){
+        return actor1.pos.x + actor1.size.x > actor2.pos.x &&
+               actor1.pos.x < actor2.pos.x + actor2.size.x &&
+               actor1.pos.y + actor1.size.y > actor2.pos.y &&
+               actor1.pos.y < actor2.pos.y + actor2.size.y;
+    }
+
+    Lava.prototype.collide = function(state){
+        let filtered = state.actors.filter(a=> a!=this);
+        let status = state.status;
+        if (!filtered.some(a=>a.type=="coin")) status ="won";
+        return new State(state.level, filtered, status);
+    }
+
+};

08245e37ca63902bb9ade120eacdf5cc6debc904
MechanoG <glory_to_the_machine@protonmail.com>
2025-07-07 23:47:51 -0400

    first error

--- JAVASCRIPT/WEBSITE/Project_Pixel_Art_Editor/index.js
+++ JAVASCRIPT/WEBSITE/Project_Pixel_Art_Editor/index.js
@@ -163,3 +163,68 @@ class ToolSelect{
     syncState(state){this.select.value = state.tool;}
 }
 
+class ColorSelect{
+    constructor(state, {dispatch}){
+        this.input = elt("input", {
+            type: "color",
+            value: state.color,
+            onchange: () => dispatch({color: this.input.value})
+        });
+        this.dom = elt ("label", null, "🎨 Color: ", this.input);
+    }
+    syncState(state) {this.input.value = state.color;}
+}
+
+//////////DrawingTools////////////
+function draw(pos, state, dispatch){
+    function drawPixel({x,y}, state){
+        let drawn = {x, y, color: state.color};
+        dispatch({picture: state.picture.draw([drawn])});
+    }
+    drawPixel(pos, state);
+    return drawPixel;
+}
+
+function rectangle(start, state, dispatch){
+    function drawRectangle(pos){
+        let xStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let xEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
+        let drawn = [];
+        for (let y = yStart; y <= yEnd; y++){
+            for (let x = xStart; x <= xEnd; x++){
+                drawn.push({x, y, color: state.color});
+            }
+        }
+        dispatch({picture: state.picture.drawn(drawn)});
+    }
+    drawRectangle(start);
+    return drawRectangle;
+}
+
+const around = [{dx: -1, dy:0}, {dx: 1, dy:0},
+                {dx: 0, dy:-1}, {dx: 0, dy:1}];
+
+function fill({x, y}, state, dispatch){
+    let targetColor = state.picture.pixel(x,y);
+    let draw = [{x, y, color: state.color}];
+    let visited = new Set();
+    for (let done=0; done<drawn.length; done++){
+        for (let {dx, dy} of around){
+            let x = drawn[done].x + dx, y = drawn[done].y + dy;
+            if(x >= 0 && x < state.picture.width &&
+                y >= 0 && y < state.picture.heigth &&
+                !visited.has(x + "," + y) &&
+                state.picture.pixel(x,y) == targetColor){
+                    drawn.push({x,y, color: state.color});
+                    visited.add(x + "," +y);
+            }
+        }
+    }    
+    dispatch({picture: state.picture.draw(drawn)});
+}
+
+function pick(pos, state, dispatch){
+    dispatch({color: state.picture.pixel(pos.x, pos.y)});
+}
